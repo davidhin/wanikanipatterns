f865dc15636c1d7325fcdf9f1371fadb9af9fb98
David Hin <david.hin@lifelenz.com>
2026-01-09 08:44:25 +1030

    Add prompts

---
+++ generate_prompts.rb
@@ -0,0 +1,68 @@
+# Usage example: (scrapes level 1)
+# ruby main.rb 1
+
+require 'open-uri'
+require 'nokogiri'
+require 'csv'
+require 'active_support/all'
+
+USER_AGENT = 'Mozilla/5.0'
+
+PROMPT = "
+Please help me create a picture to help me remember the following kanji. It uses mnemonic technique.
+Display the kanji on the top with its meaning, then a picture that is a combined representation of the meaning and reading mnemonic.
+Include excerpts of the mnemonic text throughout the image.
+".freeze
+
+def join_with_condensed_blanks(arr, separator: "\n")
+  collapsed =
+    arr.chunk(&:empty?).flat_map do |is_blank, chunk|
+      if is_blank
+        # keep one "" only when there were 2+ in a row
+        chunk.length.positive? ? [''] : []
+      else
+        chunk
+      end
+    end
+
+  collapsed.join(separator)
+end
+
+def main(url)
+  html = URI.open(url, 'User-Agent' => USER_AGENT).read
+  doc  = Nokogiri::HTML(html)
+
+  header = doc.css('.page-header').text.split.join(' ')
+  radicals = doc.css('.subject-list__items').first.children.map(&:text).map { _1.split }.select { _1.length > 1 }
+  meaning = doc.css('.subject-section--meaning').children.map { _1.text.strip }.map { join_with_condensed_blanks(_1.split("\n").map(&:strip), separator: "\n") }.select(&:present?)
+  reading = doc.css('.subject-section--reading').children.map { _1.text.strip }.map { join_with_condensed_blanks(_1.split("\n").map(&:strip), separator: "\n") }.select(&:present?)
+
+  "
+#{PROMPT}
+
+#{header}
+
+The kanji is composed of three radicals. Can you see where the radicals fit in the kanji?
+#{radicals.join(' ')}
+
+#{meaning.join("\n")}
+
+#{reading.join("\n")}
+"
+end
+
+if __FILE__ == $PROGRAM_NAME
+  level = ARGV.first || 1
+  html = URI.open("https://www.wanikani.com/level/#{level}", 'User-Agent' => USER_AGENT).read
+  doc  = Nokogiri::HTML(html)
+  urls = doc.css('.subject-character--kanji').map { _1['href'] }
+  rows = []
+  urls.each_with_index do |url, i|
+    data = main(url)
+    rows.append(data)
+    pp "Completed: #{i + 1}/#{urls.size}"
+  end
+
+  # Write rows to txt file
+  File.write("prompts_#{level}.txt", rows.join("\n------------------------------------------------------------------\n"))
+end
